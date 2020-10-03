8784efa5da228d43e8b86219e31fdc5264d6bd84
RyanParsons <ryan@jrparsonsllc.com>
2020-10-02 19:30:38 -0600

    adds expenses table component

---
+++ FinanceApp.Angular/src/app/expenses/expenses-table.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'expenses-table',
+    templateUrl: './expenses/expenses-table.component.html'
+})
+export class ExpensesTableComponent{
+
+}

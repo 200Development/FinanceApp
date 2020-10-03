5ad2acda9a4cd08d6ab6341e8b028edc3c1aefe8
RyanParsons <ryan@jrparsonsllc.com>
2020-10-02 18:21:52 -0600

    adds accounts table component

---
+++ FinanceApp.Angular/src/app/accounts/accounts-table.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'accounts-table',
+    templateUrl: './accounts/accounts-table.component.html'
+})
+export class AccountsTableComponent{
+
+}

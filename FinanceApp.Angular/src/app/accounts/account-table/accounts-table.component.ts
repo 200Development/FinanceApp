02dc86680d8289ae9fa7f6ae8a16d740ae91ee6e
Ryan Parsons <ryan.parsons@dts-global.com>
2021-02-26 23:00:39 -0700

    Updates accounts table to get data from database and styles table like expenses

--- FinanceApp.Angular/src/app/accounts/account-table/accounts-table.component.ts
+++ FinanceApp.Angular/src/app/accounts/account-table/accounts-table.component.ts
@@ -1,28 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../shared/account.service';
 import { Account } from '../shared/account';
 import { MatTableDataSource } from '@angular/material/table';
-
-const ELEMENT_DATA: Account[] = [
-    { name: 'Savings', balance: 1079, requiredSavings: 30, balanceLimit: null, balanceSurplus: 30, paycheckContribution: 15, id: 1, isCashAccount: true, isEmergencyFund: false, suggestedPaycheckContribution: 15 },
-    { name: 'E-Velocity Checking', balance: 79, requiredSavings: 30, balanceLimit: null, balanceSurplus: 30, paycheckContribution: 15, id: 1, isCashAccount: true, isEmergencyFund: false, suggestedPaycheckContribution: 15 },
-    { name: 'Marcus', balance: 5079, requiredSavings: 30, balanceLimit: null, balanceSurplus: 30, paycheckContribution: 15, id: 1, isCashAccount: true, isEmergencyFund: true, suggestedPaycheckContribution: 15 },
-];
 
 @Component({
     selector: 'accounts-table',
     templateUrl: './accounts-table.component.html',
     styleUrls: ['./accounts-table.component.css']
 })
-export class AccountsTableComponent {
+export class AccountsTableComponent implements OnInit {
 
     constructor(private accountService: AccountService) { }
 
-    displayedColumns: string[] = ['name', 'balance'];
-    dataSource = new MatTableDataSource(ELEMENT_DATA)
+    dataSource = new MatTableDataSource<Account>();
+    columnsToDisplay = ['name', 'balance'];
+    
+
+
+    ngOnInit() {
+        this.getCashAccounts();
+    }
+
+    getCashAccounts() {
+        this.accountService.getCashAccounts().subscribe((accounts: Account[]) => {
+            this.dataSource.data = accounts;
+        });
+    }
 
     applyFilter(event: Event) {
         const filterValue = (event.target as HTMLInputElement).value;
         this.dataSource.filter = filterValue.trim().toLowerCase();
       } 
+
+      editAccount(account: any) {
+        console.log(account);
+      }
 }

dc0717f0645fff215db9bd3c72fad17a71bfa16c
Ryan Parsons <ryan.parsons@dts-global.com>
2021-02-28 00:07:24 -0700

    adds disposable income component

--- FinanceApp.Angular/src/app/metrics/disposable-income/disposable-income.component.ts
+++ FinanceApp.Angular/src/app/metrics/disposable-income/disposable-income.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from 'src/app/accounts/shared/account.service';
+import { ExpenseService } from 'src/app/expenses/shared/expense.service';
 
 @Component({
   selector: 'disposable-income',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DisposableIncomeComponent implements OnInit {
 
-  constructor() { }
+  totalCash: number = 0;
+  totalLiability: number = 0;
+  disposableIncome: number = 0; 
+
+  constructor(private accountService: AccountService, private expenseService: ExpenseService) { }
 
   ngOnInit(): void {
+    this.getDisposableIncome();
   }
 
+  getDisposableIncome() {
+    this.accountService.getCashAccounts().subscribe(accounts => {
+      accounts.forEach(account => {
+        this.totalCash += account.balance;
+        this.disposableIncome = this.totalCash - this.totalLiability;
+      })
+    });
+
+    this.expenseService.getAmortizedExpenses().subscribe(expenses => {
+      expenses.forEach(expense => {
+        this.totalLiability += expense.requiredSavings;
+        this.disposableIncome = this.totalCash - this.totalLiability;
+      })
+    });
+
+    
+  }
 }

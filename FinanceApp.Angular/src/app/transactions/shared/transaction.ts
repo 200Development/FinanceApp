164d0d7fb759ab2fc868da82ab9eea997a31d8cb
Ryan Parsons <ryan.parsons@dts-global.com>
2021-02-28 22:26:05 -0700

    update transaction.Category to use categories from database

--- FinanceApp.Angular/src/app/transactions/shared/transaction.ts
+++ FinanceApp.Angular/src/app/transactions/shared/transaction.ts
@@ -1,9 +1,12 @@
+import { Category } from "src/app/expenses/shared/category";
+
 export class Transaction {
     id: number;
     userId: string;
     date: Date;
     payee: string;
     amount: number;   
-    category: any;
+    category: Category;
+    categoryId: number;
     type: any;
 }

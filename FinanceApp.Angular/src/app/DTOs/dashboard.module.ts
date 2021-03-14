6111a6932399c75072ca62b8afa417635a18a28d
Ryan Parsons <ryan.parsons@dts-global.com>
2021-03-14 01:42:17 -0700

    modifies file location for dashboard and transaction modules

--- FinanceApp.Angular/src/app/DTOs/dashboard.module.ts
+++ FinanceApp.Angular/src/app/DTOs/dashboard.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
-import { DashboardPageComponent } from './dashboard-page.component';
+import { DashboardPageComponent } from '../dashboard/dashboard-page.component';
 import { SharedModule } from '../shared.module';
-import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardRoutingModule } from '../dashboard/dashboard-routing.module';
 import { NavigationModule } from '../navigation/navigation.module';
 import { ReactiveFormsModule } from '@angular/forms/';
-import { TransactionModule } from '../transactions/shared/transaction.module';
+import { TransactionModule } from '../transactions/shared/transactions.module';
 import { ExpensesModule } from '../expenses/shared/expenses.module';
 import { MetricsModule } from '../metrics/metrics.module';
 
